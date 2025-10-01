2b91954a5af93143f54d8b55b866ac4d126ab8e1
Vem Nagarjuna Reddy <vemnagarjunareddy@gmail.com>
2025-10-01 15:16:41 +0530

    authentication and nodemailer setup

--- server/server.js
+++ server/server.js
@@ -1,18 +1,23 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 import connectDB from "./configs/db.js"
+import authRouter from "./routes/authRoutes.js"
 
-
-connectDB()
 const app = express()
-app.use(cors())
+connectDB()
 
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials:true}))
+const PORT = process.env.PORT || 3000
+
+//API Endpoints
 app.get("/",(req,res)=>{
     res.send("API is working")
 })
-
-const PORT = process.env.PORT || 3000
+app.use("/api/auth",authRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
