d3f7134d31db3521f695d7289c21ca6973ea5c44
Vem Nagarjuna Reddy <vemnagarjunareddy@gmail.com>
2025-10-09 19:36:14 +0530

    port update server

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // API Endpoints
 app.get("/", (req, res) => {
