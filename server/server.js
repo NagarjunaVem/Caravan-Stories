222c12f4ad0a0fa2c8b64c6369010f7ccf47f407
Vem Nagarjuna Reddy <vemnagarjunareddy@gmail.com>
2025-10-09 20:17:28 +0530

    server.js fix

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ import profileRouter from "./routes/profileRoutes.js";
 import roleRequestRouter from "./routes/roleRequestRoutes.js";
 import statsRouter from "./routes/statsRoutes.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 connectDB();
 
@@ -21,6 +24,7 @@ const allowedOrigins = ['http://localhost:5173','https://caravan-stories.vercel.
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins,methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], credentials: true }));
+app.options("*",cors())
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
